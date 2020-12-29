6e15ba103a7dd7ead3482839e161bb537ed6872a
ShameBidar <ma85.bahar@gmail.com>
2020-12-29 17:30:06 +0330

    first implement

--- HARD_DETECTION/mainwindow.cpp
+++ HARD_DETECTION/mainwindow.cpp
@@ -5,14 +5,13 @@
 #include <QStringListModel>
 #include <mntent.h>
 
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QStringList listing;
-    QStringListModel *model = new QStringListModel(/*some QObject * as a parent, e.g. ui->ListPic */ ui->listView);
-
     struct mntent *ent;
     FILE *aFile;
 
@@ -21,16 +20,29 @@ MainWindow::MainWindow(QWidget *parent)
       perror("setmntent");
       exit(1);
     }
-    while (NULL != (ent = getmntent(aFile))) {
-        char arr[400];
-        sprintf(arr, "%s    %s    %s", ent->mnt_fsname, ent->mnt_type ,ent->mnt_dir);
-        if(strstr(ent->mnt_fsname,"/sd"))
+    int i = 0;
+    while (NULL != (ent = getmntent(aFile)) && i<2) {
+
+        if(strstr(ent->mnt_fsname,"/sd") && strcmp(ent->mnt_dir,"/") != 0)
         {
-            listing << QString::fromLocal8Bit(arr);
+            //blisting << QString::fromLocal8Bit(arr);
+            if(i == 0)
+            {
+                ui->btn_disk_1->setText(ent->mnt_dir);
+                first_disk.str_dev = ent->mnt_fsname;
+                first_disk.str_filesys = ent->mnt_type;
+
+            }
+            else if(i == 1)
+            {
+                ui->btn_disk_2->setText(ent->mnt_dir);
+                second_disk.str_dev = ent->mnt_fsname;
+                second_disk.str_filesys = ent->mnt_type;
+            }
+            i++;
+
         }
     }
-    model->setStringList(listing);
-    ui->listView->setModel(model);
     endmntent(aFile);
 
 
@@ -41,3 +53,43 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_btn_disk_1_clicked()
+{
+    ui->lbl_device->setText(first_disk.str_dev);
+    ui->lbl_filesys->setText(first_disk.str_filesys);
+    which_btn = SELECT_FIRST;
+}
+
+void MainWindow::on_btn_disk_2_clicked()
+{
+    ui->lbl_device->setText(second_disk.str_dev);
+    ui->lbl_filesys->setText(second_disk.str_filesys);
+    which_btn = SELECT_SECOND;
+}
+
+/***********************
+ * dev='/dev/sdb'
+sudo umount "$dev"
+printf "o\nn\np\n1\n\n\nw\n" | sudo fdisk "$dev"
+sudo mkfs.ext4 "${dev}1"
+ * *********************/
+void MainWindow::on_btn_format_clicked()
+{
+    QString dev;
+    if(which_btn == SELECT_FIRST)
+    {
+        dev = first_disk.str_dev;
+    }
+    else if(which_btn == SELECT_SECOND)
+    {
+        dev = second_disk.str_dev;
+    }
+    if(which_btn == SELECT_FIRST || which_btn == SELECT_SECOND)
+    {
+        //QString data;
+        //data.toStdString().c_str();
+        QString cmd = QString("umount %1").arg(dev);
+        system(cmd.toStdString().c_str());
+    }
+}
--- HARD_DETECTION/mainwindow.h
+++ HARD_DETECTION/mainwindow.h
@@ -9,17 +9,38 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+class DevModel
+{
+public:
+    QString str_filesys;
+    QString str_dev;
+};
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    enum WHICH_BTN
+    {
+        SELECT_FIRST,
+        SELECT_SECOND,
+    };
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
 
+    void on_btn_disk_1_clicked();
+
+    void on_btn_disk_2_clicked();
+
+    void on_btn_format_clicked();
+
 private:
     Ui::MainWindow *ui;
+    DevModel first_disk;
+    DevModel second_disk;
+    WHICH_BTN which_btn;
 };
 #endif // MAINWINDOW_H
