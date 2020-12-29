887e5b56212a38c513ca51717f9dca2b9b9cf94c
ShameBidar <ma85.bahar@gmail.com>
2020-12-29 15:45:04 +0330

    find mountpoint

--- HARD_DETECTION/mainwindow.cpp
+++ HARD_DETECTION/mainwindow.cpp
@@ -1,11 +1,39 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDirIterator>
+#include <QDebug>
+#include <QStringListModel>
+#include <mntent.h>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QStringList listing;
+    QStringListModel *model = new QStringListModel(/*some QObject * as a parent, e.g. ui->ListPic */ ui->listView);
+
+    struct mntent *ent;
+    FILE *aFile;
+
+    aFile = setmntent("/proc/mounts", "r");
+    if (aFile == NULL) {
+      perror("setmntent");
+      exit(1);
+    }
+    while (NULL != (ent = getmntent(aFile))) {
+        char arr[400];
+        sprintf(arr, "%s    %s    %s", ent->mnt_fsname, ent->mnt_type ,ent->mnt_dir);
+        if(strstr(ent->mnt_fsname,"/sd"))
+        {
+            listing << QString::fromLocal8Bit(arr);
+        }
+    }
+    model->setStringList(listing);
+    ui->listView->setModel(model);
+    endmntent(aFile);
+
+
 }
 
 MainWindow::~MainWindow()
--- HARD_DETECTION/mainwindow.h
+++ HARD_DETECTION/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +16,8 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
 
 private:
     Ui::MainWindow *ui;
